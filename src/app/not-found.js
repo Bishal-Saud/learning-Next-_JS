726d9deda34ff56595cbbb1be991919c1938a724
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-01-12 22:22:11 +0545

    conditional style and css 1

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,13 +1,18 @@
+"use client";
 import Link from "next/link";
 import style from "./page.module.css";
 import clear from "./style/other.module.css";
+import { useState } from "react";
 export default function NotFound() {
+  const [color, setColor] = useState("red");
   return (
-    <div className={style.error}>
+    <div className={color === "red" ? style.red : style.blue}>
       <h1>This page is not available !</h1>
       <Link className={clear.beauty} href="/login">
         Go back to login
       </Link>
+
+      <button onClick={() => setColor("blue")}>change color</button>
     </div>
   );
 }

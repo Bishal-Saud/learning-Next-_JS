546aae882927a4cc77a114dd10076f5685cad5ad
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-01-13 13:45:38 +0545

    conditional style and css 2

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,14 +5,24 @@ import clear from "./style/other.module.css";
 import { useState } from "react";
 export default function NotFound() {
   const [color, setColor] = useState("red");
+  const { borderX } = style;
   return (
     <div className={color === "red" ? style.red : style.blue}>
-      <h1>This page is not available !</h1>
+      <h1 style={{ backgroundColor: color == "red" ? "red" : "whitesmoke" }}>
+        This page is not available !
+      </h1>
       <Link className={clear.beauty} href="/login">
         Go back to login
       </Link>
+      <button id={style.fontSize} onClick={() => setColor("blue")}>
+        change color
+      </button>
 
-      <button onClick={() => setColor("blue")}>change color</button>
+      <h2 className={borderX}>Dummy text</h2>
+      <h2 className={borderX}>Dummy text</h2>
+      <h2 className={borderX}>Dummy text</h2>
+      <h2 className={borderX}>Dummy text</h2>
+      <h2 className={borderX}>Dummy text</h2>
     </div>
   );
 }

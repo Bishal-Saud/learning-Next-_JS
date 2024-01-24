9dbe07d599866fa41ab95ad1a67421e88f1342f4
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-01-24 11:02:21 +0545

    redirect in next js

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/users",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/users/:id",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;

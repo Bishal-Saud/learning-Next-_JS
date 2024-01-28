0b07315cdb94a634e475676a4c941782ac559945
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-01-28 22:04:04 +0545

    Make API with Post Method

--- my-app/src/app/api/user/route.js
+++ my-app/src/app/api/user/route.js
@@ -5,3 +5,17 @@ export function GET(request, content) {
   // console.log(content);
   return NextResponse.json(user, { status: 200 });
 }
+
+export async function POST(req) {
+  const payload = await req.json();
+  console.log(payload.name);
+
+  if (!payload.name || !payload.email || !payload.age) {
+    return NextResponse.json({ result: "Data not Found" }, { status: 404 });
+  }
+
+  return NextResponse.json(
+    { result: "result successfully found!" },
+    { status: 202 }
+  );
+}
